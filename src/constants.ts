1840f736c4dc9103ed30edbb62f2802060a06efb
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2023-01-26 10:34:00 +0300

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const EXTENSION_NAME = "CSS To TailwindCSS converter";
+export const CONFIG_FILE_GLOB =
+  "{tailwind,tailwind.config,tailwind.*.config,tailwind.config.*}.{js,cjs,mjs,ts,cts,mts}";
